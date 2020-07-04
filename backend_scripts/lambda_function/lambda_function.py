c68e7b87fbb8f6a904969c1e0150440a7b83be74
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-07-04 18:53:08 +1000

    changed json output

--- backend_scripts/lambda_function/lambda_function.py
+++ backend_scripts/lambda_function/lambda_function.py
@@ -2,12 +2,18 @@ import json
 import serpapi_script as ss
 
 def lambda_handler(event, context):
-    # TODO implement
-    test_image = ss.serpapi_webpage('https://s1.at.atcdn.net/wp-content/uploads/2018/09/Uluru_hero-768x369.jpg')
-    search_term = test_image.related_search_term()
-
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(search_term)
-    }
+    # Error checking
+    try:
+        test_image = ss.serpapi_webpage(event['image_url'])
+        search_term = test_image.get_related_search_term()
+        no_results = test_image.get_no_total_results()
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'related_search_term':search_term,'total_no_results':no_results}, 
+                                sort_keys=True, indent=4)
+        }
+    except:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Image search failed')
+        }
