79073e3a1644378956f2f08c4c2500e7e9818a06
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-07-04 19:51:54 +1000

    small modifications to add error chechecking and better comments

--- backend_scripts/lambda_function/serpapi_script.py
+++ backend_scripts/lambda_function/serpapi_script.py
@@ -15,13 +15,13 @@ class serpapi_webpage:
     def __init__(self, image_url):
         
         self.image_url = image_url
-        self.api_key = "insert_api_key"
+        self.api_key = "add_key"
         # Editing link given in for webpage search using REGEX
         p = re.compile('/')
         self.new_url = p.sub("%2F", self.image_url)
         p = re.compile(':')
         self.new_url = p.sub("%3A", self.new_url)
-        self.new_url = "https://serpapi.com/search.html?engine=google_reverse_image&image_url={}&&api_key={}".format(self.new_url, self.api_key)
+        self.new_url = "https://serpapi.com/search.html?engine=google_reverse_image&image_url={}&api_key={}".format(self.new_url, self.api_key)
     
     def get_no_total_results(self):
 
@@ -39,7 +39,7 @@ class serpapi_webpage:
         result = p.sub("", result)
         return int(result)
     
-    def related_search_term(self):
+    def get_related_search_term(self):
         
         # Get webpage
         response = request.urlopen(self.new_url).read()
@@ -52,18 +52,3 @@ class serpapi_webpage:
         p = re.compile('<.*')
         result = p.sub("", result)
         return result
-
-"""
-# Testing code, algorithm time varies, sometimes quick, sometimes not (~4s).
-# Minor optimisations maybe required, or the server has slowed down.
-# Could add this on with TinEye as an option since TinEye does not have
-# this particular functionality.
-
-test_link = None
-time_1 = time.perf_counter()
-extra_image_info = scrape_webpage('https://i0.wp.com/metro.co.uk/wp-content/uploads/2019/09/PRI_85898263.jpg?quality=90&strip=all&zoom=1&resize=644%2C475&ssl=1')
-print(extra_image_info.related_search_term())
-print(extra_image_info.get_no_total_results())
-time_2 = time.perf_counter()
-print(time_2-time_1)
-"""
