fc97e5988f8cc01662100472aef843b7db181233
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-07-06 14:08:56 +1000

    added changes

--- backend_scripts/lambda_function/serpapi_script.py
+++ backend_scripts/lambda_function/serpapi_script.py
@@ -15,21 +15,22 @@ class serpapi_webpage:
     def __init__(self, image_url):
         
         self.image_url = image_url
-        self.api_key = "add_key"
+        self.api_key = "9103e042e9ebe7ee57d0f91a3a457519932ea82aaf69a778f57721b215c26ff4"
         # Editing link given in for webpage search using REGEX
         p = re.compile('/')
         self.new_url = p.sub("%2F", self.image_url)
         p = re.compile(':')
         self.new_url = p.sub("%3A", self.new_url)
         self.new_url = "https://serpapi.com/search.html?engine=google_reverse_image&image_url={}&api_key={}".format(self.new_url, self.api_key)
-    
+        # Optimization made -> load request and soup before analysis
+        self.response = request.urlopen(self.new_url).read()
+        self.soup = BeautifulSoup(self.response,'html.parser')
+
+
     def get_no_total_results(self):
 
-        # Get webpage
-        response = request.urlopen(self.new_url).read()
-        soup = BeautifulSoup(response,'lxml')
         # Gather html code containing number of searches
-        result = str(soup.find(id = 'result-stats'))
+        result = str(self.soup.find(id = 'result-stats'))
         # Use REGEX to modify string into returnable number
         p = re.compile('.+s">')
         result = p.sub("", result)
@@ -41,11 +42,8 @@ class serpapi_webpage:
     
     def get_related_search_term(self):
         
-        # Get webpage
-        response = request.urlopen(self.new_url).read()
-        soup = BeautifulSoup(response,'lxml')
         # Gather html code containing number of searches
-        result = str(soup.find(class_ = 'fKDtNb'))
+        result = str(self.soup.find(class_ = 'fKDtNb'))
         # Use REGEX to get desirable string
         p = re.compile('.*italic">')
         result = p.sub("", result)
