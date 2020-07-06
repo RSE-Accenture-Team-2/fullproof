323cc49ec2f2a19c600b14686798c7af62c61baf
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-07-06 14:06:11 +1000

    fixed output

--- backend_scripts/lambda_function_edit.py
+++ backend_scripts/lambda_function_edit.py
@@ -9,14 +9,18 @@ import serpapi_script as ss
 def lambda_handler(event, context):
     
     # Basic error test, need to detect false urls being added.
-    # Also add some time function to enable except to be executed
+    # Also add some time function to enable except to be executed       
     try:
         test_image = ss.serpapi_webpage(event['image_url'])
         search_term = test_image.get_related_search_term()
         no_results = test_image.get_no_total_results()
+        image_info = {
+            "related_search_term":search_term,
+            "total_no_results":no_results
+        }
         return {
             'statusCode': 200,
-            'body': {"related_search_term":search_term,"total_no_results":no_results}
+            'body': json.dumps(image_info)
         }
     except:
         return {
