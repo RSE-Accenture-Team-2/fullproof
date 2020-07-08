5c448656ecf5830ea53a15898d0dcfd606ccd7b9
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2020-07-08 18:52:54 +1000

    Made function better but still not working with API

--- backend_scripts/ELAFunction/env/lib/python3.8/site-packages/lambda_function.py
+++ backend_scripts/ELAFunction/env/lib/python3.8/site-packages/lambda_function.py
@@ -1,19 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import traceback
 
 def lambda_handler(event, context):
-    jsondata = json.loads(event["body"])
-    url = jsondata["url"]
-    data = { 'url' : url }
-    base_url = 'http://fotoforensics.com'
-    response = requests.post(f'{ base_url }/upload-url.php',data=data)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    img_endpoint = soup.find(id="MainImg")
-    img_endpoint = img_endpoint.attrs["src"]
-    img_endpoint = str(img_endpoint).split("&")[0] + "&fmt=ela"
-    dns = base_url + img_endpoint
-    return {
-    "statusCode": 200,
-    "body": json.dumps({ "ELAlink" : dns })
-}
+    try:
+        if type(event) is dict:
+            dumped = json.dumps(event)
+            event = str(dumped)
+
+        jsondata = json.loads(str(event))
+        print(type(jsondata))
+        print(jsondata)
+
+        url = jsondata['body']['url']
+        data = { 'url' : url }
+        base_url = 'http://fotoforensics.com'
+        response = requests.post(f'{ base_url }/upload-url.php',data=data)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        img_endpoint = soup.find(id="MainImg")
+        img_endpoint = img_endpoint.attrs['src']
+        img_endpoint = str(img_endpoint).split("&")[0] + "&fmt=ela"
+        dns = base_url + img_endpoint
+        return {
+        "statusCode": 200,
+        "body": { 'ELAlink' : dns }
+    }
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+        return {
+            'statusCode' : 500, 
+            'body' : json.dumps(traceback.format_exc())
+ }
