70ec662a25676b280c8affaae6f619f5e5fcce3f
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2020-07-08 19:20:16 +1000

    convert to urllib

--- backend_scripts/ELAFunction/env/lib/python3.8/site-packages/lambda_function.py
+++ backend_scripts/ELAFunction/env/lib/python3.8/site-packages/lambda_function.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-import requests
+import urllib.parse
+import urllib.request
 import json
 import traceback
 
@@ -14,11 +15,17 @@ def lambda_handler(event, context):
         print(jsondata)
 
         url = jsondata['body']['url']
-        data = { 'url' : url }
+        # data = { 'url' : url }
         base_url = 'http://fotoforensics.com'
-        response = requests.post(f'{ base_url }/upload-url.php',data=data)
+
+        url = base_url
+        full_url = url + '?url=' + url
+        response = urllib.request.urlopen(full_url)
+
+        # response = requests.post(f'{ base_url }/upload-url.php',data=data)
         print(response)
-        soup = BeautifulSoup(response.text, 'html.parser')
+
+        soup = BeautifulSoup(response, 'html.parser')
         img_endpoint = soup.find(id="MainImg")
         img_endpoint = img_endpoint.attrs['src']
         img_endpoint = str(img_endpoint).split("&")[0] + "&fmt=ela"
@@ -35,3 +42,8 @@ def lambda_handler(event, context):
             'statusCode' : 500, 
             'body' : json.dumps(traceback.format_exc())
  }
+
+event = { "body" : { "url" : "https://i.insider.com/5ea8734ca34b3c340e3bd55a?width=1800&format=jpeg&auto=webp" } }
+context = None
+result = lambda_handler(event, context)
+print(result)
