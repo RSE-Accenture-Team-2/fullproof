859e80f73ff5bd5a6253cb5e01bd02c2b7375873
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2020-07-09 19:11:49 +1000

    ELA lambda

--- backend_scripts/ELAFunction/env/lib/python3.8/site-packages/lambda_function.py
+++ backend_scripts/ELAFunction/env/lib/python3.8/site-packages/lambda_function.py
@@ -2,34 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import traceback
-import re
+
 
 def lambda_handler(event, context):
     try:
+        print(event)
         if type(event) is not dict:
             eventdata = json.loads(event)
         else:
             eventdata = event
-
+        print(eventdata)
         url = eventdata["body"]['url']
-
-        data = { 'url' : url }
+        data = {'url': url}
         base_url = 'http://fotoforensics.com'
-        
-        response = requests.post(f'{ base_url }/upload-url.php',data=data)
+        response = requests.post(
+            f'{ base_url }/upload-url.php', data=data, timeout=30)
+        print(response.status_code)
         soup = BeautifulSoup(response.text, 'html.parser')
         img_endpoint = soup.find(id="MainImg")
         img_endpoint = img_endpoint.attrs["src"]
         img_endpoint = str(img_endpoint).split("&")[0] + "&fmt=ela"
         dns = base_url + img_endpoint
         return {
-        "statusCode": 200,
-        "body": json.dumps({'ELAlink' : dns })
-    }
+            "statusCode": 200,
+            "body": json.dumps({'ELAlink': dns})
+        }
     except Exception as e:
-        print(e)
-        print(traceback.format_exc())
         return {
-            "statusCode" : 500, 
-            "body" : json.dumps(traceback.format_exc())
- }
+            "statusCode": 500,
+            "body": json.dumps(traceback.format_exc())
+        }
+
+
+event = '{ "body" : { "url" : "https://i.insider.com/5ea8734ca34b3c340e3bd55a?width=1800&format=jpeg&auto=webp" } }'
+context = None
+result = lambda_handler(event, context)
+print(result)
